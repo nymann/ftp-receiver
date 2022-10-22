32df320f225e2c2d8070266f7665b8fb0241ff9e
Kristian Nymann Jakobsen <kristian@nymann.dev>
2022-10-23 01:37:33 +0200

    :construction: WIP

--- src/ftp_receiver/main.py
+++ src/ftp_receiver/main.py
@@ -5,14 +5,22 @@ import sys
 import time
 
 from ftp_receiver.config import Config
+from ftp_receiver.consumer import ExistingFilesKafkaConsumer
 from ftp_receiver.ftp_client import FTPClient
 from ftp_receiver.publisher import KafkaPublisher
 
 
 def main() -> None:
     config = Config()
+    try:
+        run_until_cancelled(config=config, publisher=KafkaPublisher(config=config))
+    except KeyboardInterrupt:
+        logging.info("Interrupted by keyboard")
+
+
+def run_until_cancelled(config: Config, publisher: KafkaPublisher) -> None:
     logging.basicConfig(stream=sys.stdout, level=config.log.level)
-    publisher = KafkaPublisher(config=config)
+
     interval = config.download.interval_seconds
     while True:  # noqa: WPS457 infinite-while-loop
         logging.info(f"Running FTP Receiver v{config.version}")
@@ -24,6 +32,9 @@ def main() -> None:
 def run(config: Config, publisher: KafkaPublisher) -> None:
     client = FTPClient(config=config)
     downloaded = filenames(output=config.download.directory)
+    for missing in missing_files(config=config):
+        publisher.publish(filename=missing)
+
     for filename in sorted(client.list()):
         if filename in downloaded:
             continue
@@ -31,6 +42,16 @@ def run(config: Config, publisher: KafkaPublisher) -> None:
             client.download(filename=filename)
             publisher.publish(filename=filename)
     client.quit()
+
+
+def missing_files(config: Config) -> list[str]:
+    published_filenames_consumer = ExistingFilesKafkaConsumer(config=config)
+    missing = filenames(output=config.download.directory)
+    for published_filename in published_filenames_consumer.consume():
+        if published_filename in missing:
+            missing.remove(published_filename)
+    logging.info("missing files added: %d", len(missing))
+    return sorted(missing)
 
 
 def filenames(output: Path) -> set[str]:
--- src/ftp_receiver/publisher.py
+++ src/ftp_receiver/publisher.py
@@ -1,4 +1,8 @@
-from kafka import KafkaProducer
+import logging
+
+from confluent_kafka import KafkaError
+from confluent_kafka import Message
+from confluent_kafka import Producer
 
 from ftp_receiver.config import Config
 
@@ -9,12 +13,20 @@ class KafkaPublisher:
         self.key = config.ftp.host.encode("utf-8")
         host = config.kafka.host
         port = config.kafka.port
-        self.producer = KafkaProducer(bootstrap_servers=f"{host}:{port}")
+        self.producer = Producer({"bootstrap.servers": f"{host}:{port}"})
 
     def publish(self, filename: str) -> None:
-        self.producer.send(
+        self.producer.poll(0)
+        self.producer.produce(
             topic=self.topic,
             key=self.key,
             value=filename.encode("utf-8"),
+            on_delivery=self._on_delivery,
         )
         self.producer.flush()
+
+    def _on_delivery(self, error: KafkaError, message: Message) -> None:
+        if error:
+            logging.error("Failed to publish.")
+        else:
+            logging.debug("Published %s successfully", message.key())
